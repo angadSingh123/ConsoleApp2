df888c217751530b225e70d220feedfcca9a8981
Harsimrat Singh <singh21.h@gmail.com>
2019-05-04 15:49:01 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        const int PORT_DEFAULT = 8080;
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 3 || args.Length < 2) throw new ArgumentException("Usage: ConsoleApp2 <server> [<port>] <message>");
+
+            String server = (args[0]);
+
+            int servPort = (args.Length == 3) ? Int32.Parse(args[1]) : PORT_DEFAULT;
+
+            byte[] buffer = Encoding.ASCII.GetBytes(args[args.Length - 1]);
+
+            UdpClient client = null;
+
+            try
+            {
+
+                client = new UdpClient();
+
+                client.Send(buffer, buffer.Length, server, servPort);
+
+                IPEndPoint endp = new IPEndPoint(IPAddress.Any, servPort);
+
+
+
+
+            }
+            catch (SocketException e) {
+
+                Environment.Exit(e.ErrorCode);
+
+            }
+
+            client.Close();
+
+        }
+    }
+}
